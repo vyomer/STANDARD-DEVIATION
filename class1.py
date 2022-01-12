a06cc8305dd07fd600599dcc6150fa152210d33e
vyomer <75195848+vyomer@users.noreply.github.com>
2022-01-12 14:45:37 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,31 @@
+import csv
+from numpy.core.fromnumeric import shape
+
+with open('Standard_deviation-master\graphs\class1.csv',newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+file_data.pop(0)
+total_marks = 0
+total_entries = len(file_data)
+for marks in file_data:
+    total_marks += float(marks[1])
+mean = total_marks/total_entries
+print(str(mean))
+    
+import pandas as pd
+
+import plotly.express as px
+
+df = pd.read_csv("Standard_deviation-master\graphs\class1.csv")
+fig = px.scatter(df,x = "Student Number",y = "Marks")
+fig.update_layout(shapes = [
+    dict(
+        type = 'line',
+        y0 = mean,
+        y1 = mean,
+        x0 = 0,
+        x1 = total_entries
+    )
+])
+fig.show()
---
+++ class2.py
@@ -0,0 +1,37 @@
+import csv
+
+with open('Standard_deviation-master\graphs\class2.csv', newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+#To remove headers from CSV
+file_data.pop(0)
+
+total_marks = 0
+total_entries = len(file_data)
+
+for marks in file_data:
+    total_marks += float(marks[1])
+
+mean = total_marks / total_entries
+print("Mean (Average) is -> "+str(mean))
+
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("Standard_deviation-master\graphs\class2.csv")
+
+fig = px.scatter(df,    x="Student Number",
+                        y="Marks"
+            )
+fig.update_layout(shapes=[
+    dict(
+      type= 'line',
+      y0= mean, y1= mean,
+      x0= 0, x1= total_entries
+    )
+])
+
+fig.update_yaxes(rangemode="tozero")
+
+fig.show()
